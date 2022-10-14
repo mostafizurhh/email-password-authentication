1d72811387a63106590802e2d976e5aff7e728cd
Mostafiz <mostafiz@iMac-Mostafizur.lan>
2022-10-14 10:28:17 +0100

    created User With Email And Password in firebase with simple validation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import { getAuth } from 'firebase/auth';
 import './App.css';
-import app from './firebase/firebase.init';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import RegisterReactBootstrap from './components/Register/RegisterReactBootstrap';
 import RegisterBootstarp from './components/Register/RegisterBootstarp';
 import RegisterTailwind from './components/Register/RegisterTailwind';
+import { createUserWithEmailAndPassword, getAuth } from 'firebase/auth';
+import app from './firebase/firebase.init';
 
 const auth = getAuth(app);
 
@@ -13,6 +13,14 @@ const handleRegister = (event) => {
   const email = event.target.email.value;
   const password = event.target.password.value;
   console.log(email, password);
+  createUserWithEmailAndPassword(auth, email, password)
+    .then(result => {
+      const user = result.user;
+      console.log(user)
+    })
+    .catch(error => {
+      console.error('error: ', error)
+    })
 }
 
 const handleEmailOnChange = event => {
@@ -27,25 +35,37 @@ function App() {
   return (
     <div>
 
-      {/* simple html */}
-      <form onSubmit={handleRegister} className='mx-auto w-50'>
+      {/*----------------
+          simple html
+        ----------------- */}
+      {/* <form onSubmit={handleRegister} className='mx-auto w-50'>
         <input onChange={handleEmailOnChange} type="email" name="email" id="" placeholder='Enter your email' />
         <br /> <br />
         <input onBlur={handlePasswordOnBlur} type="password" name="password" id="" placeholder='Eter your password' />
         <br /> <br />
         <button type="submit">Register</button>
-      </form>
-      {/* React Bootstrap */}
+      </form> */}
+
+      {/*----------------------
+         React Bootstrap
+        -----------------------*/}
       <RegisterReactBootstrap
         handleRegister={handleRegister}
       ></RegisterReactBootstrap>
-      {/* Bootstrap */}
-      <RegisterBootstarp
+
+      {/*------------------------
+          Bootstrap
+        -------------------------*/}
+      {/* <RegisterBootstarp
         handleRegister={handleRegister}
-      ></RegisterBootstarp>
-      <RegisterTailwind
+      ></RegisterBootstarp> */}
+
+      {/*------------------------
+          Tailwind
+        -------------------------*/}
+      {/* <RegisterTailwind
         handleRegister={handleRegister}
-      ></RegisterTailwind>
+      ></RegisterTailwind> */}
     </div>
   );
 }
--- src/components/Register/RegisterReactBootstrap.js
+++ src/components/Register/RegisterReactBootstrap.js
@@ -2,24 +2,22 @@ import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 
-
 const RegisterReactBootstrap = ({ handleRegister }) => {
+
     return (
         <div className='w-50 mx-auto mt-5'>
             <h3>Please Register!!!!</h3>
             <Form onSubmit={handleRegister}>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" name='email' placeholder="Enter email" />
+                    <Form.Control type="email" name='email' placeholder="Enter email" required />
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" name='password' placeholder="Password" />
+                    <Form.Control type="password" name='password' placeholder="Password" required />
                 </Form.Group>
-                <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                    <Form.Check type="checkbox" label="Check me out" />
-                </Form.Group>
+
                 <Button variant="primary" type="submit">
                     Register
                 </Button>
